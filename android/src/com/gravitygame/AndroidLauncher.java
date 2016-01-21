f7552adce74928293cfaa60991c019b963022fcb
Sawyer Harris <sawyerh99@yahoo.com>
2016-01-20 20:01:28 -0500

    First commit

---
+++ android/src/com/gravitygame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.gravitygame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.gravitygame.GravityGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GravityGame(), config);
+	}
+}
